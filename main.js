526f67430f55811cfd69e0448a96a0a97a1f78ac
AnmolAqsa <165940283+AnmolAqsa@users.noreply.github.com>
2024-07-04 12:27:13 -0700

    Add files via upload
    Q9, 45 Projects

---
+++ main.js
@@ -0,0 +1,7 @@
+/*
+Q9. Favorite Number: Store your favorite number in a variable. Then, using that variable, create a
+ message that reveals your favorite number. Print that message.
+*/
+var favoriteNumer = 26;
+var message = "My Favorite Numer is " + favoriteNumer;
+console.log(message);
---
+++ main.ts
@@ -0,0 +1,8 @@
+/*
+Q9. Favorite Number: Store your favorite number in a variable. Then, using that variable, create a
+ message that reveals your favorite number. Print that message.
+*/
+
+let favoriteNumer =  26;
+let message = "My Favorite Numer is " +favoriteNumer;
+console.log(message);   
